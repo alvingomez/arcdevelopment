185359f087570d87c947f8780fea8de384179ecc
Alvin Gomez <alvingomez.world@gmail.com>
2021-06-29 00:04:35 +0800

    Button Typography
    Added to modified theme to be used later or on other components.

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -3,6 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const arcBlue = "#0B72B9"
 const arcOrange ="#FFBA60"
 
+//Creates an instance of the default MUI and then essentially
+//tells is that I want to overwrite any of the default values
+//with the object that I am going to pass witin it. 
 export default createMuiTheme({
     palette:{
         common:{
@@ -17,8 +20,19 @@ export default createMuiTheme({
         }
     }, 
     typography: {
-      h3:{
-          fontWeight:300
-      }
+        tab:{
+            fontFamily:"Raleway",
+            //To disable the uppercasing due to the styling of the button component 
+            //being used underneath the tab component. 
+            textTransform:"none", 
+            fontWeight:700,
+            fontSize:"1rem"
+        },
+        estimate:{
+            fontFamily: "Roboto",
+            fontSize: "15px",
+            textTransform: "none",
+            color:"white"
+        }, 
     }
 })

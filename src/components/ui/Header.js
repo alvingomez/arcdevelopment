c9903a26544392b82b14cc57aab846e082561ae5
Alvin Gomez <alvingomez.world@gmail.com>
2021-07-05 23:13:39 +0800

    Logo Ripple effect and updating button value
    Remove the ripple effect and updated the state value when clicking the home logo.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -6,8 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
 
@@ -37,7 +36,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "3em",
   },
   logo: {
-    height: "7em",
+    height: "8em",
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover":{
+      backgroundColor:"transparent"
+    }
   },
   tabContainer: {
     marginLeft: "auto",
@@ -58,7 +63,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
- function Header({ props }) {
+function Header({ props }) {
   //This JSS is compiled into actual CSS.
   const classes = useStyles();
   const [value, setValue] = useState(0);
@@ -67,45 +72,78 @@ const useStyles = makeStyles((theme) => ({
     setValue(value);
   };
 
-    useEffect(() => {
+  console.log(value);
+  useEffect(() => {
     /**The window.location object can be used to get the current page address(URL)
-     * and redirect the browser to a new page. 
+     * and redirect the browser to a new page.
      * (window.location.pathname) returns the path and filename of the current page
      */
-    if(window.location.pathname === "/" && value !==0){
-     setValue(0);
-    }else if(window.location.pathname === "/services" && value !==1){
-     setValue(1);
-    }else if(window.location.pathname === "/revolution" && value !==2){
-      setValue(2)
-    }else if(window.location.pathname === "/about" && value !==3){
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
       setValue(3);
-    }else if(window.location.pathname === "/contact" && value !==4){
+    } else if (window.location.pathname === "/contact" && value !== 4) {
       setValue(4);
-    }else if (window.location.pathname === "/estimate" && value !==5){
-      setValue(5)
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
     }
-    /*if the state "value" has not changed then I don't want to run this code */
+    console.log(value);
+    /*if the state "value" has not changed then I don't want to run this code again*/
   }, [value]);
-
 
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img alt="company logo" src={logo} className={classes.logo} />
+            <Button disableRipple component={Link} to="/" className={classes.logoContainer}>
+              <img
+                alt="company logo"
+                src={logo}
+                className={classes.logo}
+                onClick={() => setValue(0)}
+              />
+            </Button>
             <Tabs
               value={value}
               className={classes.tabContainer}
               onChange={handleChange}
               indicatorColor="primary"
             >
-              <Tab className={classes.tab} component={Link} to="/" label="Home" />
-              <Tab className={classes.tab} component={Link} to="/services" label="Services" />
-              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
-              <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
-              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
             </Tabs>
             <Button
               variant="contained"
